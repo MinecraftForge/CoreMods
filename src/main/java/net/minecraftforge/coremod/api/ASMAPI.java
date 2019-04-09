8a6c47938db98c0833e62a9280775d7e3f97c77c
Tobias Hotz <tobias.hotz@hotmail.de>
2019-04-09 18:48:02 +0200

    Add methods to remap methods and fields, fixes #10 Allow instantiation of InsnList, fixes #13

--- src/main/java/net/minecraftforge/coremod/api/ASMAPI.java
+++ src/main/java/net/minecraftforge/coremod/api/ASMAPI.java
@@ -1,5 +1,7 @@
 package net.minecraftforge.coremod.api;
 
+import cpw.mods.modlauncher.Launcher;
+import cpw.mods.modlauncher.api.INameMappingService;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.*;
 
@@ -18,5 +20,17 @@ public class ASMAPI {
 
     public static MethodInsnNode buildMethodCall(final String ownerName, final String methodName, final String methodDescriptor, final MethodType type) {
         return new MethodInsnNode(type.ordinal()+Opcodes.INVOKEVIRTUAL, ownerName, methodName, methodDescriptor, type==MethodType.INTERFACE);
+    }
+
+    public static String mapMethod(String name) {
+        return map(name, INameMappingService.Domain.METHOD);
+    }
+
+    public static String mapField(String name) {
+        return map(name, INameMappingService.Domain.FIELD);
+    }
+
+    private static String map(String name, INameMappingService.Domain domain) {
+        return Launcher.INSTANCE.environment().findNameMapping("srg").map(f -> f.apply(domain, name)).orElse(name);
     }
 }
