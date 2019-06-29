240e429d8b6c9ac5b3652f3d1802b06a03872e72
Tobias Hotz <tobias.hotz@hotmail.de>
2019-06-29 23:51:31 +0200

    Remove targetAll parameter of insertInsnList - it does not work that way, as ASM clear the list after insert and saves states in the nodes, with no possibility to clone the list before. Add some tests for new ASMAPI methods

--- src/testJars/java/cpw/mods/TestClass.java
+++ src/testJars/java/cpw/mods/TestClass.java
@@ -12,4 +12,19 @@ public class TestClass {
     public static void main(String... args) {
         cheese();
     }
+
+    public static boolean testInsert() {
+        testMethod1();
+        boolean result = testMethod2();
+        testMethod1();
+        return result;
+    }
+
+    public static long testMethod1() {
+        return (long) (Math.random() * 3290L);
+    }
+
+    public static boolean testMethod2() {
+        return true;
+    }
 }
