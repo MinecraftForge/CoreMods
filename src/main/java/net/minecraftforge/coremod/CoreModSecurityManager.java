44551e747ea676ce143d32f5fba99198bd4b3354
Mahtaran <mahtaran@amuzil.com>
2019-08-09 09:07:46 +0200

    Convert even more tabs to spaces

--- src/main/java/net/minecraftforge/coremod/CoreModSecurityManager.java
+++ src/main/java/net/minecraftforge/coremod/CoreModSecurityManager.java
@@ -3,36 +3,36 @@ package net.minecraftforge.coremod;
 import java.security.Permission;
 
 public class CoreModSecurityManager extends SecurityManager {
-		private final SecurityManager oldSecurityManager;
+    private final SecurityManager oldSecurityManager;
 
-		public CoreModSecurityManager(SecurityManager oldSecurityManager) {
-			this.oldSecurityManager = oldSecurityManager;
-		}
+    public CoreModSecurityManager(SecurityManager oldSecurityManager) {
+        this.oldSecurityManager = oldSecurityManager;
+    }
 
-		@Override
-		public void checkConnect(String host, int port) {
-			throw constructException(host, port);
-		}
+    @Override
+    public void checkConnect(String host, int port) {
+        throw constructException(host, port);
+    }
 
-		@Override
-		public void checkConnect(String host, int port, Object context) {
-			throw constructException(host, port);
-		}
-		
-		private SecurityException constructException(String host, int port) {
-			String url = host + (port > -1 ? ":" + port" : "");
-			return new SecurityException("Connection to " + url + " was blocked; loading remote scripts is not allowed.");
-		}
+    @Override
+    public void checkConnect(String host, int port, Object context) {
+        throw constructException(host, port);
+    }
+        
+    private SecurityException constructException(String host, int port) {
+        String url = host + (port > -1 ? ":" + port" : "");
+        return new SecurityException("Connection to " + url + " was blocked; loading remote scripts is not allowed.");
+    }
 
-		@Override
-		public void checkPermission(Permission perm) {
-			if (oldSecurityManager != null)
-				oldSecurityManager.checkPermission(perm);
-		}
+    @Override
+    public void checkPermission(Permission perm) {
+        if (oldSecurityManager != null)
+            oldSecurityManager.checkPermission(perm);
+    }
 
-		@Override
-		public void checkPermission(Permission perm, Object context) {
-			if (oldSecurityManager != null)
-				oldSecurityManager.checkPermission(perm, context);
-		}
-	}
+    @Override
+    public void checkPermission(Permission perm, Object context) {
+        if (oldSecurityManager != null)
+            oldSecurityManager.checkPermission(perm, context);
+    }
+}
